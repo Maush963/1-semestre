38dc701c90ee859c4e58d1b6a4b2595211c6d03c
Maush963 <111213737+Maush963@users.noreply.github.com>
2022-10-05 20:37:28 -0500

    Update Matrices tridimensionales.py

--- Matrices tridimensionales.py
+++ Matrices tridimensionales.py
@@ -1,101 +1,105 @@
 mesi=3
-usuariosd=0
 diai=10
-diasmes=31
-meses=[[[1,"enero",usuariosd],[2,"enero",usuariosd],[3,"enero",usuariosd],[4,"enero",usuariosd],[5,"enero",usuariosd],[6,"enero",usuariosd],[7,"enero",usuariosd],[8,"enero",usuariosd],[9,"enero",usuariosd],[10,"enero",usuariosd],
-            [11,"enero",usuariosd],[12,"enero",usuariosd],[13,"enero",usuariosd],[14,"enero",usuariosd],[15,"enero",usuariosd],[16,"enero",usuariosd],[17,"enero",usuariosd],[18,"enero",usuariosd],[19,"enero",usuariosd],[20,"enero",usuariosd],
-            [22,"enero",usuariosd],[22,"enero",usuariosd],[23,"enero",usuariosd],[24,"enero",usuariosd],[25,"enero",usuariosd],[26,"enero",usuariosd],[27,"enero",usuariosd],[28,"enero",usuariosd],[29,"enero",usuariosd],[30,"enero",usuariosd],
-            [31,"enero",usuariosd]],
+mesf=6
+diaf=12
+usuariosc=4 #Cantidad total de involucrados
+
+meses=[[[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[10,1],
+            [11,1],[12,1],[13,1],[14,1],[15,1],[16,1],[17,1],[18,1],[19,1],[20,1],
+            [22,1],[22,1],[23,1],[24,1],[25,1],[26,1],[27,1],[28,1],[29,1],[30,1],
+            [31,1]],
     
-           [[1,"febrero",usuariosd],[2,"febrero",usuariosd],[3,"febrero",usuariosd],[4,"febrero",usuariosd],[5,"febrero",usuariosd],[6,"febrero",usuariosd],[7,"febrero",usuariosd],[8,"febrero",usuariosd],[9,"febrero",usuariosd],[10,"febrero",usuariosd],
-           [11,"febrero",usuariosd],[12,"febrero",usuariosd],[13,"febrero",usuariosd],[14,"febrero",usuariosd],[15,"febrero",usuariosd],[16,"febrero",usuariosd],[17,"febrero",usuariosd],[18,"febrero",usuariosd],[19,"febrero",usuariosd],[20,"febrero",usuariosd],
-           [22,"febrero",usuariosd],[22,"febrero",usuariosd],[23,"febrero",usuariosd],[24,"febrero",usuariosd],[25,"febrero",usuariosd],[26,"febrero",usuariosd],[27,"febrero",usuariosd],[28,"febrero",usuariosd]],
+           [[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2],[9,2],[10,2],
+           [11,2],[12,2],[13,2],[14,2],[15,2],[16,2],[17,2],[18,2],[19,2],[20,2],
+           [22,2],[22,2],[23,2],[24,2],[25,2],[26,2],[27,2],[28,2]],
         
-           [[1,"marzo",usuariosd],[2,"marzo",usuariosd],[3,"marzo",usuariosd],[4,"marzo",usuariosd],[5,"marzo",usuariosd],[6,"marzo",usuariosd],[7,"marzo",usuariosd],[8,"marzo",usuariosd],[9,"marzo",usuariosd],[10,"marzo",usuariosd],
-           [11,"marzo",usuariosd],[12,"marzo",usuariosd],[13,"marzo",usuariosd],[14,"marzo",usuariosd],[15,"marzo",usuariosd],[16,"marzo",usuariosd],[17,"marzo",usuariosd],[18,"marzo",usuariosd],[19,"marzo",usuariosd],[20,"marzo",usuariosd],
-           [22,"marzo",usuariosd],[22,"marzo",usuariosd],[23,"marzo",usuariosd],[24,"marzo",usuariosd],[25,"marzo",usuariosd],[26,"marzo",usuariosd],[27,"marzo",usuariosd],[28,"marzo",usuariosd],[29,"marzo",usuariosd],[30,"marzo",usuariosd],
-           [31,"marzo",usuariosd]],
+           [[1,3],[2,3],[3,3],[4,3],[5,3],[6,3],[7,3],[8,3],[9,3],[10,3],
+           [11,3],[12,3],[13,3],[14,3],[15,3],[16,3],[17,3],[18,3],[19,3],[20,3],
+           [22,3],[22,3],[23,3],[24,3],[25,3],[26,3],[27,3],[28,3],[29,3],[30,3],
+           [31,3]],
         
-           [[1,"abril",usuariosd],[2,"abril",usuariosd],[3,"abril",usuariosd],[4,"abril",usuariosd],[5,"abril",usuariosd],[6,"abril",usuariosd],[7,"abril",usuariosd],[8,"abril",usuariosd],[9,"abril",usuariosd],[10,"abril",usuariosd],
-           [11,"abril",usuariosd],[12,"abril",usuariosd],[13,"abril",usuariosd],[14,"abril",usuariosd],[15,"abril",usuariosd],[16,"abril",usuariosd],[17,"abril",usuariosd],[18,"abril",usuariosd],[19,"abril",usuariosd],[20,"abril",usuariosd],
-           [22,"abril",usuariosd],[22,"abril",usuariosd],[23,"abril",usuariosd],[24,"abril",usuariosd],[25,"abril",usuariosd],[26,"abril",usuariosd],[27,"abril",usuariosd],[28,"abril",usuariosd],[29,"abril",usuariosd],[30,"abril",usuariosd],
+           [[1,4],[2,4],[3,4],[4,4],[5,4],[6,4],[7,4],[8,4],[9,4],[10,4],
+           [11,4],[12,4],[13,4],[14,4],[15,4],[16,4],[17,4],[18,4],[19,4],[20,4],
+           [22,4],[22,4],[23,4],[24,4],[25,4],[26,4],[27,4],[28,4],[29,4],[30,4],
            ],
  
-           [[1,"mayo",usuariosd],[2,"mayo",usuariosd],[3,"mayo",usuariosd],[4,"mayo",usuariosd],[5,"mayo",usuariosd],[6,"mayo",usuariosd],[7,"mayo",usuariosd],[8,"mayo",usuariosd],[9,"mayo",usuariosd],[10,"mayo",usuariosd],
-           [11,"mayo",usuariosd],[12,"mayo",usuariosd],[13,"mayo",usuariosd],[14,"mayo",usuariosd],[15,"mayo",usuariosd],[16,"mayo",usuariosd],[17,"mayo",usuariosd],[18,"mayo",usuariosd],[19,"mayo",usuariosd],[20,"mayo",usuariosd],
-           [22,"mayo",usuariosd],[22,"mayo",usuariosd],[23,"mayo",usuariosd],[24,"mayo",usuariosd],[25,"mayo",usuariosd],[26,"mayo",usuariosd],[27,"mayo",usuariosd],[28,"mayo",usuariosd],[29,"mayo",usuariosd],[30,"mayo",usuariosd],
-           [31,"mayo",usuariosd]],
+           [[1,5],[2,5],[3,5],[4,5],[5,5],[6,5],[7,5],[8,5],[9,5],[10,5],
+           [11,5],[12,5],[13,5],[14,5],[15,5],[16,5],[17,5],[18,5],[19,5],[20,5],
+           [22,5],[22,5],[23,5],[24,5],[25,5],[26,5],[27,5],[28,5],[29,5],[30,5],
+           [31,5]],
         
-           [[1,"junio",usuariosd],[2,"junio",usuariosd],[3,"junio",usuariosd],[4,"junio",usuariosd],[5,"junio",usuariosd],[6,"junio",usuariosd],[7,"junio",usuariosd],[8,"junio",usuariosd],[9,"junio",usuariosd],[10,"junio",usuariosd],
-           [11,"junio",usuariosd],[12,"junio",usuariosd],[13,"junio",usuariosd],[14,"junio",usuariosd],[15,"junio",usuariosd],[16,"junio",usuariosd],[17,"junio",usuariosd],[18,"junio",usuariosd],[19,"junio",usuariosd],[20,"junio",usuariosd],
-           [22,"junio",usuariosd],[22,"junio",usuariosd],[23,"junio",usuariosd],[24,"junio",usuariosd],[25,"junio",usuariosd],[26,"junio",usuariosd],[27,"junio",usuariosd],[28,"junio",usuariosd],[29,"junio",usuariosd],[30,"junio",usuariosd],
+           [[1,6],[2,6],[3,6],[4,6],[5,6],[6,6],[7,6],[8,6],[9,6],[10,6],
+           [11,6],[12,6],[13,6],[14,6],[15,6],[16,6],[17,6],[18,6],[19,6],[20,6],
+           [22,6],[22,6],[23,6],[24,6],[25,6],[26,6],[27,6],[28,6],[29,6],[30,6],
            ],
         
-           [[1,"julio",usuariosd],[2,"julio",usuariosd],[3,"julio",usuariosd],[4,"julio",usuariosd],[5,"julio",usuariosd],[6,"julio",usuariosd],[7,"julio",usuariosd],[8,"julio",usuariosd],[9,"julio",usuariosd],[10,"julio",usuariosd],
-           [11,"julio",usuariosd],[12,"julio",usuariosd],[13,"julio",usuariosd],[14,"julio",usuariosd],[15,"julio",usuariosd],[16,"julio",usuariosd],[17,"julio",usuariosd],[18,"julio",usuariosd],[19,"julio",usuariosd],[20,"julio",usuariosd],
-           [22,"julio",usuariosd],[22,"julio",usuariosd],[23,"julio",usuariosd],[24,"julio",usuariosd],[25,"julio",usuariosd],[26,"julio",usuariosd],[27,"julio",usuariosd],[28,"julio",usuariosd],[29,"julio",usuariosd],[30,"julio",usuariosd],
-           [31,"julio",usuariosd]],
+           [[1,7],[2,7],[3,7],[4,7],[5,7],[6,7],[7,7],[8,7],[9,7],[10,7],
+           [11,7],[12,7],[13,7],[14,7],[15,7],[16,7],[17,7],[18,7],[19,7],[20,7],
+           [22,7],[22,7],[23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],
+           [31,7]],
         
-           [[1,"agosto",usuariosd],[2,"agosto",usuariosd],[3,"agosto",usuariosd],[4,"agosto",usuariosd],[5,"agosto",usuariosd],[6,"agosto",usuariosd],[7,"agosto",usuariosd],[8,"agosto",usuariosd],[9,"agosto",usuariosd],[10,"agosto",usuariosd],
-           [11,"agosto",usuariosd],[12,"agosto",usuariosd],[13,"agosto",usuariosd],[14,"agosto",usuariosd],[15,"agosto",usuariosd],[16,"agosto",usuariosd],[17,"agosto",usuariosd],[18,"agosto",usuariosd],[19,"agosto",usuariosd],[20,"agosto",usuariosd],
-           [22,"agosto",usuariosd],[22,"agosto",usuariosd],[23,"agosto",usuariosd],[24,"agosto",usuariosd],[25,"agosto",usuariosd],[26,"agosto",usuariosd],[27,"agosto",usuariosd],[28,"agosto",usuariosd],[29,"agosto",usuariosd],[30,"agosto",usuariosd],
-           [31,"agosto",usuariosd]],
+           [[1,8],[2,8],[3,8],[4,8],[5,8],[6,8],[7,8],[8,8],[9,8],[10,8],
+           [11,8],[12,8],[13,8],[14,8],[15,8],[16,8],[17,8],[18,8],[19,8],[20,8],
+           [22,8],[22,8],[23,8],[24,8],[25,8],[26,8],[27,8],[28,8],[29,8],[30,8],
+           [31,8]],
         
-           [[1,"septiembre",usuariosd],[2,"septiembre",usuariosd],[3,"septiembre",usuariosd],[4,"septiembre",usuariosd],[5,"septiembre",usuariosd],[6,"septiembre",usuariosd],[7,"septiembre",usuariosd],[8,"septiembre",usuariosd],[9,"septiembre",usuariosd],[10,"septiembre",usuariosd],
-           [11,"septiembre",usuariosd],[12,"septiembre",usuariosd],[13,"septiembre",usuariosd],[14,"septiembre",usuariosd],[15,"septiembre",usuariosd],[16,"septiembre",usuariosd],[17,"septiembre",usuariosd],[18,"septiembre",usuariosd],[19,"septiembre",usuariosd],[20,"septiembre",usuariosd],
-           [22,"septiembre",usuariosd],[22,"septiembre",usuariosd],[23,"septiembre",usuariosd],[24,"septiembre",usuariosd],[25,"septiembre",usuariosd],[26,"septiembre",usuariosd],[27,"septiembre",usuariosd],[28,"septiembre",usuariosd],[29,"septiembre",usuariosd],[30,"septiembre",usuariosd],
+           [[1,9],[2,9],[3,9],[4,9],[5,9],[6,9],[7,9],[8,9],[9,9],[10,9],
+           [11,9],[12,9],[13,9],[14,9],[15,9],[16,9],[17,9],[18,9],[19,9],[20,9],
+           [22,9],[22,9],[23,9],[24,9],[25,9],[26,9],[27,9],[28,9],[29,9],[30,9],
            ],
         
-           [[1,"octubre",usuariosd],[2,"octubre",usuariosd],[3,"octubre",usuariosd],[4,"octubre",usuariosd],[5,"octubre",usuariosd],[6,"octubre",usuariosd],[7,"octubre",usuariosd],[8,"octubre",usuariosd],[9,"octubre",usuariosd],[10,"octubre",usuariosd],
-           [11,"octubre",usuariosd],[12,"octubre",usuariosd],[13,"octubre",usuariosd],[14,"octubre",usuariosd],[15,"octubre",usuariosd],[16,"octubre",usuariosd],[17,"octubre",usuariosd],[18,"octubre",usuariosd],[19,"octubre",usuariosd],[20,"octubre",usuariosd],
-           [22,"octubre",usuariosd],[22,"octubre",usuariosd],[23,"octubre",usuariosd],[24,"octubre",usuariosd],[25,"octubre",usuariosd],[26,"octubre",usuariosd],[27,"octubre",usuariosd],[28,"octubre",usuariosd],[29,"octubre",usuariosd],[30,"octubre",usuariosd],
-           [31,"octubre",usuariosd]],
+           [[1,10],[2,10],[3,10],[4,10],[5,10],[6,10],[7,10],[8,10],[9,10],[10,10],
+           [11,10],[12,10],[13,10],[14,10],[15,10],[16,10],[17,10],[18,10],[19,10],[20,10],
+           [22,10],[22,10],[23,10],[24,10],[25,10],[26,10],[27,10],[28,10],[29,10],[30,10],
+           [31,10]],
         
-           [[1,"noviembre",usuariosd],[2,"noviembre",usuariosd],[3,"noviembre",usuariosd],[4,"noviembre",usuariosd],[5,"noviembre",usuariosd],[6,"noviembre",usuariosd],[7,"noviembre",usuariosd],[8,"noviembre",usuariosd],[9,"noviembre",usuariosd],[10,"noviembre",usuariosd],
-           [11,"noviembre",usuariosd],[12,"noviembre",usuariosd],[13,"noviembre",usuariosd],[14,"noviembre",usuariosd],[15,"noviembre",usuariosd],[16,"noviembre",usuariosd],[17,"noviembre",usuariosd],[18,"noviembre",usuariosd],[19,"noviembre",usuariosd],[20,"noviembre",usuariosd],
-           [22,"noviembre",usuariosd],[22,"noviembre",usuariosd],[23,"noviembre",usuariosd],[24,"noviembre",usuariosd],[25,"noviembre",usuariosd],[26,"noviembre",usuariosd],[27,"noviembre",usuariosd],[28,"noviembre",usuariosd],[29,"noviembre",usuariosd],[30,"noviembre",usuariosd],
+           [[1,11],[2,11],[3,11],[4,11],[5,11],[6,11],[7,11],[8,11],[9,11],[10,11],
+           [11,11],[12,11],[13,11],[14,11],[15,11],[16,11],[17,11],[18,11],[19,11],[20,11],
+           [22,11],[22,11],[23,11],[24,11],[25,11],[26,11],[27,11],[28,11],[29,11],[30,11],
            ],
         
-           [[1,"diciembre",usuariosd],[2,"diciembre",usuariosd],[3,"diciembre",usuariosd],[4,"diciembre",usuariosd],[5,"diciembre",usuariosd],[6,"diciembre",usuariosd],[7,"diciembre",usuariosd],[8,"diciembre",usuariosd],[9,"diciembre",usuariosd],[10,"diciembre",usuariosd],
-           [11,"diciembre",usuariosd],[12,"diciembre",usuariosd],[13,"diciembre",usuariosd],[14,"diciembre",usuariosd],[15,"diciembre",usuariosd],[16,"diciembre",usuariosd],[17,"diciembre",usuariosd],[18,"diciembre",usuariosd],[19,"diciembre",usuariosd],[20,"diciembre",usuariosd],
-           [22,"diciembre",usuariosd],[22,"diciembre",usuariosd],[23,"diciembre",usuariosd],[24,"diciembre",usuariosd],[25,"diciembre",usuariosd],[26,"diciembre",usuariosd],[27,"diciembre",usuariosd],[28,"diciembre",usuariosd],[29,"diciembre",usuariosd],[30,"diciembre",usuariosd],
-           [31,"diciembre",usuariosd]],
+           [[1,12],[2,12],[3,12],[4,12],[5,12],[6,12],[7,12],[8,12],[9,12],[10,12],
+           [11,12],[12,12],[13,12],[14,12],[15,12],[16,12],[17,12],[18,12],[19,12],[20,12],
+           [22,12],[22,12],[23,12],[24,12],[25,12],[26,12],[27,12],[28,12],[29,12],[30,12],
+           [31,12]]
  
-           ] #enero febrero marzo abril mayo junio julio agosto septiembre octubre noviembre diciembre
-"""
+           ] #enero febrero marzo abril mayo junio julio agosto septiembre octubre noviembre
+
+#FUNCION PARA DETERMINAR USUARIOS QUE VAN A IR/DIA
 def mes (meses):
+    acum=0
+    usuariosd=0 #Cantidad de usuarios que si van a ir
+    asis=0
+    
+#este for toma en cuenta todo un mes, range=12
     for cont in meses:
+#este for toma en cuenta toda la información, pero no lo que tiene en su interior
         for info in cont:
-            for diau in info:
-                if(diau<mesi AND diau<diai):
-                    if (diau>mesf AND diau>diaf):
-                        asis=input(print(f"¿Puede asistir a la reunion este día? {diau}\t, si("s")/no("n"): ")
-                        if(asis=="s"):
+#con estos dos if, me aseguro de que este dentro del rango del intervalo
+            if(info[1]>=mesi and info[0]>=diai):
+                if (info[1]<=mesf and info[0]<=diaf):
+#en este while estoy preguntando a todos los usuarios si pueden ir
+                    while (acum<usuariosc):
+                        asis=int(input(f"¿Puede asistir a la reunion este día? {info[0]}, si(1)/no(0): "))
+#aqui esta iterando la cantidad de usuarios que si pueden asistir a la reunion con un IF, lueog lo imprime
+                        if(asis!=0):
                             usuariosd=+ asis
-                        print(f"Estos son los usuarios que pueden asistir este día")
-                        
-                        
-
-
-
-"""
-def mes (meses):
-    for cont in meses:
-        print (f"{cont}")
-        if cont==mesi:
-            print ("su mes es marzo")
-        for info in cont:
-            print(f"{info}")
-            for diau in info:
-                print(f"({diau})",end=" ")
-                if diau==diai:
-                    tiempo=diasmes-diai
-                    print("LOTERIAA")
-            print("\n")
-        
-    print (tiempo)
-
+                            print(usuariosd)
+                        acum=+1
+                    porcentaje=(usuariosd/usuariosc)*100
+                    print("este es el porcentaje" ,porcentaje)
+                    
 
 #aqui estoy encontrando el día que digito el usuario para restarle el día que digito a los dias del mes, de esta forma lo voy a sumar más tarde con los días de otros meses
 
 mes(meses)
+
+"""
+def intro ():
+    print("Bienvenido a when2meet pirata :D")
+    mesi=input("Introduzca el mes inicial para la reunion ---> ")
+    diai=input("Introduzca el día inicial para la reunion ---> ")
+    mesf=input("Introduzca el mes final para la reunion ---> ")
+    diaf=input("Introduzca el día final para la reunion ---> ")
+    usuariosc=inout("introduzca la cantidad de asistentes para esta reunion ---> ")
+"""    
